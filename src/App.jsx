19c71c289e0f71009a002cf964506b78244177c4
badr ghanbi <bghanbi50@gmail.com>
2023-07-04 12:20:10 +0100

    add some general typography styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>$3,102.04</h1>
+      <h2>Invoices</h2>
+      <h3>05 Nov 2021</h3>
+      <p>19 Union Terrace
+
+        London
+
+        E1 3EZ
+
+        United Kingdom</p>
+    </div>
   )
 }
 
