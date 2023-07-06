519538cab12313e8ff63f1a76b80846bec29367e
badr ghanbi <bghanbi50@gmail.com>
2023-07-06 16:15:45 +0100

    clean up work environment a little bitt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Navbar from "./componenet/Navbar/Navbar"
-import EditOrDeleteInvoiceBox from "./componenet/pages/invoiceDetails/EditOrDeleteInvoiceBox"
 const App = () => {
   return (
     <>
       <Navbar />
-      <EditOrDeleteInvoiceBox />
     </>
 
   )
