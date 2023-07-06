17c6af491497fa84c8b077c8b75dd560d548e8d0
badr ghanbi <bghanbi50@gmail.com>
2023-07-06 12:51:32 +0100

    refactor the removeOrEdit componenet a little

--- src/componenet/pages/invoiceDetails/EditOrDeleteInvoiceBox.jsx
+++ src/componenet/pages/invoiceDetails/EditOrDeleteInvoiceBox.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Card from '../../ui/Card'
 import InvoiceStatus from '../../ui/InvoiceStatus'
 import { Button } from '../../ui/Button'
+const flexStyle = "flex items-center"
 const EditOrDeleteInvoiceBox = () => {
     return (
         <Card className="mt-5 flex justify-between items-center">
-            <div className='flex items-center'>
+            <div className={flexStyle}>
                 <p className='mr-7'>Status</p>
                 <InvoiceStatus />
             </div>
-            <div className='flex items-center'>
+            <div className={flexStyle}>
                 <Button text='edit' className="bg-darkBlue mr-3" />
                 <Button text='delete' className="bg-paleRed" />
             </div>
