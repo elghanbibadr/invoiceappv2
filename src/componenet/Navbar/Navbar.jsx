7a6c6747a5969d942184de09cd827dc413d52269
badr ghanbi <bghanbi50@gmail.com>
2023-07-06 12:17:04 +0100

    create the base layout for the navbar componenet

--- src/componenet/Navbar/Navbar.jsx
+++ src/componenet/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import logo from "../../../public/assets/logo.svg"
+import sunIcon from "../../../public/assets/icon-sun.svg"
+import avatar from "../../../public/assets/image-avatar.jpg"
 
 const Navbar = () => {
     return (
         <header>
-            <nav className="nav bg-primaryColor flex justify-between items-center">
-                <div className="nav__logo-wrapper  flex items-center justify-center h-20 w-20 bg-paleCyan">
+            <nav className="nav bg-primaryColor flex justify-between items-center pr-5">
+                <div className="nav__logo-wrapper  flex items-center justify-center h-20 w-20 bg-accentColor">
                     <img src={logo} alt="navbar logo" className="nav__logo " />
                 </div>
                 <div className="navLogos-Wrraper flex flex-row justify-between items-center">
-                    <img className='nav__them mr-10 ' src={sunLogo} />
-                    <img className='nav__avatar rounded-full h-10' src={avatar} />
+                    <img className='nav__them mr-10 ' src={sunIcon} />
+                    <img className='nav__avatar rounded-full h-8' src={avatar} />
                 </div>
             </nav>
         </header>
