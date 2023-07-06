7d7400f4298bb41c0d4e924d52e842118de55c71
badr ghanbi <bghanbi50@gmail.com>
2023-07-06 10:28:04 +0100

    create a resuable button componenet

--- src/componenet/ui/Button.jsx
+++ src/componenet/ui/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Button = () => {
     return (
-        <div>Button</div>
+        <button className={`${className} rounded-full text-[0.8rem]  leading-[0.93rem] capitalize text-white font-bold  py-[1rem] px-[1.5rem]`}>delete</button>
     )
 }
