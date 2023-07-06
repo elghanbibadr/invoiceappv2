7a45d2dca922a55ab19fbf72fd80a43f1a9d8ab9
badr ghanbi <bghanbi50@gmail.com>
2023-07-06 16:33:18 +0100

    the desktop version got created for the invoice details componenet

--- src/componenet/pages/invoiceDetailsPage/InvoiceDetailsPage.jsx
+++ src/componenet/pages/invoiceDetailsPage/InvoiceDetailsPage.jsx
@@ -3,9 +3,9 @@ import Card from '../../ui/Card'
 
 const InvoiceDetailsPage = () => {
     return (
-        <Card>
+        <Card className="mt-10">
             <div className='grid grid-cols-2'>
-                <p className='font-bold mb-6 col-span-2'> <strong className='text-accentColor'>#</strong>TV6773</p>
+                <p className='font-bold mb-16 col-span-2 md:col-span-3'> <strong className='text-accentColor'>#</strong>TV6773</p>
                 <div>
                     <p className='mb-2'>Invoice Date</p>
                     <h3>23 Dec 2022</h3>
@@ -18,7 +18,7 @@ const InvoiceDetailsPage = () => {
                     <p className='mb-2'>Payment Due</p>
                     <h3>30 Dec 2022</h3>
                 </div>
-                <div>
+                <div className='md:col-start-3 md:row-start-2'>
                     <p className='mb-2'>Sent To</p>
                     <h3>mosh hemdanai</h3>
                 </div>
