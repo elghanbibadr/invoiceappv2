57aa3a58421e202832afd1f90a11fb1f0502be08
badr ghanbi <bghanbi50@gmail.com>
2023-07-07 13:57:29 +0100

    create an arrow that rotate when dropdown collapses and render  dropdwon conditionaliy

--- src/componenet/ui/DropDown.jsx
+++ src/componenet/ui/DropDown.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import checkIcon from "../../../public/assets/icon-check.svg"
 import Card from './Card'
+import arrow from "../../../public/assets/icon-arrow-down.svg"
 
 const dropDownItems = [
     { id: "1", text: "Draft" },
@@ -10,15 +11,21 @@ const dropDownItems = [
 ]
 const DropDown = () => {
     const [activeBox, SetActiveBox] = useState(null)
+    const [dropDownCollapsed, setDropDownCollapsed] = useState(false)
 
     const handleOptionSelected = (boxId) => {
         SetActiveBox(boxId)
     }
 
+    const handleDropDownCollapsed = () => setDropDownCollapsed(prv => !prv)
+
     return (
         <>
-            <h4>Filter by status</h4>
-            <Card className="w-fit pr-14">
+            <div className='flex  items-center' onClick={handleDropDownCollapsed}>
+                <h4 className='cursor-pointer'>Filter by status</h4>
+                <img className={`ml-2 mt-1 ${dropDownCollapsed ? "rotate-180" : "rotate-0"} transition-transform duration-300`} src={arrow} alt="arrow icon" />
+            </div>
+            {dropDownCollapsed && <Card className="w-fit pr-14">
                 {dropDownItems.map(({ id, text }) => {
                     return <div onClick={() => handleOptionSelected(id)} key={id} className='flex items-center mb-4'>
                         <span className={`h-4 w-4  rounded-sm inline-flex justify-center items-center ${activeBox === id ? ' bg-accentColor' : 'bg-secondaryColor'} `}>
@@ -27,7 +34,7 @@ const DropDown = () => {
                         <p className='font-bold ml-2 capitalize'>{text}</p>
                     </div>
                 })}
-            </Card>
+            </Card>}
         </>
     )
 }
