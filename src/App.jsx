1663856d98df0f842080ff8908d75c79316edb1a
badr ghanbi <bghanbi50@gmail.com>
2023-07-07 13:24:07 +0100

    create a custom dropdwon menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./componenet/Navbar/Navbar"
 import Container from "./componenet/ui/Container"
+
 import InvoiceDetailsPage from "./componenet/pages/invoiceDetailsPage/InvoiceDetailsPage"
+import DropDown from "./componenet/ui/DropDown"
 const App = () => {
   return (
     <>
       <Navbar />
       <Container>
-        <InvoiceDetailsPage />
+        <DropDown />
       </Container>
     </>
 
---
+++ src/componenet/ui/DropDown.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import checkIcon from "../../../public/assets/icon-check.svg"
+import Card from './Card'
+
+const dropDownItems = [
+    { id: "1", text: "Draft" },
+    { id: "2", text: "Pending" },
+    { id: "3", text: "Paid" }
+
+]
+const DropDown = () => {
+    const [activeBox, SetActiveBox] = useState(null)
+    const handleClick = (boxId) => {
+        SetActiveBox(boxId)
+        console.log(boxId)
+    }
+    return (
+        <>
+            <h4>Filter by status</h4>
+            <Card className="w-fit pr-14">
+                {dropDownItems.map(({ id, text }) => {
+                    return <div onClick={() => handleClick(id)} key={id} className='flex items-center mb-4'>
+                        <span className={`h-4 w-4  rounded-sm inline-flex justify-center items-center ${activeBox === id ? ' bg-accentColor' : 'bg-secondaryColor'} `}>
+                            {activeBox === id && <img src={checkIcon} />}
+                        </span>
+                        <p className='font-bold ml-2 capitalize'>{text}</p>
+                    </div>
+                })}
+            </Card>
+        </>
+    )
+}
+
+export default DropDown
