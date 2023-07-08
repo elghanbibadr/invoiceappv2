41c7cacc66380a6d9cf42e394c360d9775bddb8d
badr ghanbi <bghanbi50@gmail.com>
2023-07-08 13:33:57 +0100

    add a border to the avatar logo

--- src/componenet/Navbar/Navbar.jsx
+++ src/componenet/Navbar/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
         <header>
             <nav className="nav bg-primaryColor flex justify-between items-center pr-5">
                 <div className="nav__logo-wrapper  flex items-center justify-center h-20 w-20 bg-accentColor">
-                    <img src={logo} alt="navbar logo" className="nav__logo " />
+                    <img src={logo} alt="navbar logo" className="nav__logo  " />
                 </div>
                 <div className="navLogos-Wrraper flex flex-row justify-between items-center">
                     <img className='nav__them mr-10 ' src={sunIcon} />
-                    <img className='nav__avatar rounded-full h-8' src={avatar} />
+                    <img className='nav__avatar hover:border-2 border-accentColor rounded-full h-8' src={avatar} />
                 </div>
             </nav>
         </header>
