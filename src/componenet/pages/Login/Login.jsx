e24f691df946f8ecf723aa27a169a782b6144998
badr ghanbi <bghanbi50@gmail.com>
2023-07-13 13:31:06 +0100

    fetch invoices from firestore

--- src/componenet/pages/Login/Login.jsx
+++ src/componenet/pages/Login/Login.jsx
@@ -1,7 +1,8 @@
 import { signInAnonymously, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
 import { auth } from '../../../../public/firebase/FirebaseConfig';
 import { useNavigate } from 'react-router-dom';
-
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../../../../public/firebase/FirebaseConfig';
 
 const Login = () => {
 
@@ -11,15 +12,21 @@ const Login = () => {
         try {
             // Sign in anonymously
             await signInAnonymously(auth);
-            console.log(auth.currentUser)
-            // navigate user to home page
-            // if (auth.currentUser) {
-            //     navigate('/home');
-
-            // }
+            if (!auth.currentUser) return;
             // Fetch invoices for demo user
-
             // Add your logic to fetch invoices from Firestore
+            const querySnapshot = await getDocs(collection(db, 'demoinvoices'));
+            const fetchedItems = [];
+            querySnapshot.forEach((doc) => {
+                fetchedItems.push({
+                    id: doc.id,
+                    data: doc.data()
+                });
+            });
+
+            // navigate user to home page
+            navigate('/home');
+
         } catch (error) {
             console.error('Error signing in:', error);
         }
