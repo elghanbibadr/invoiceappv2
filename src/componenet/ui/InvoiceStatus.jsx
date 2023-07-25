247a950359c9b680e3565a2972aafed46a2f7e85
badr ghanbi <bghanbi50@gmail.com>
2023-07-25 16:03:16 +0100

    change the color ofthe status depending on the status value

--- src/componenet/ui/InvoiceStatus.jsx
+++ src/componenet/ui/InvoiceStatus.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const InvoiceStatus = ({ className, status }) => {
     return (
-        <div className={`flex ${className} items-center bg-primaryColor w-fit py-3 px-5 rounded-md`}>
-            <span className='h-2 w-2 rounded-full inline-block bg-white'></span>
-            <p className='font-bold text-sm ml-2'>{status}</p>
+        <div className={`flex ${className} items-center bg-primaryColor w-[100px] py-3 px-5 rounded-md`}>
+            <span className={`h-2 w-2 rounded-full inline-block ${status === "paid" ? "bg-green-400" : status === "pending" ? "bg-orange-400" : "bg-white"}`}></span>
+            <p className={`font-bold capitalize text-sm ml-2 ${status === "paid" ? "text-green-400" : status === "pending" ? "text-orange-400" : "text-white"}`}>{status}</p>
         </div>
     )
 }
