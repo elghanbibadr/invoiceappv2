1230f75f24d915c0f35fb784d1795f0ca1c94eda
badr ghanbi <bghanbi50@gmail.com>
2023-07-25 15:53:59 +0100

    pass the status props instead of the hardcoded value

--- src/componenet/ui/InvoiceStatus.jsx
+++ src/componenet/ui/InvoiceStatus.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const InvoiceStatus = ({ className }) => {
+const InvoiceStatus = ({ className, status }) => {
     return (
         <div className={`flex ${className} items-center bg-primaryColor w-fit py-3 px-5 rounded-md`}>
             <span className='h-2 w-2 rounded-full inline-block bg-white'></span>
-            <p className='font-bold text-sm ml-2'>Pending</p>
+            <p className='font-bold text-sm ml-2'>{status}</p>
         </div>
     )
 }
