f3db3af4e799866ca3de487b4dc9f00fd0566582
badr ghanbi <bghanbi50@gmail.com>
2023-08-01 14:58:28 +0100

    more testing

--- src/componenet/pages/Login/Login.jsx
+++ src/componenet/pages/Login/Login.jsx
@@ -1,13 +1,17 @@
 import { signInAnonymously, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
 import { auth } from '../../../../public/firebase/FirebaseConfig';
 import { useNavigate } from 'react-router-dom';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, query } from 'firebase/firestore';
 import { db } from '../../../../public/firebase/FirebaseConfig';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { addDoc } from 'firebase/firestore';
+import { where } from 'firebase/firestore';
+import { useEffect } from 'react';
 import { AppContext } from '../../../store/AppContext';
 
 const Login = () => {
     const { setInvoices } = useContext(AppContext)
+    const [user, setUser] = useState(undefined)
 
     const navigate = useNavigate();
 
@@ -38,17 +42,57 @@ const Login = () => {
         }
     };
 
+    // const handleSignInWithGitHub = async () => {
+    //     try {
+    //         // Sign in with GitHub
+    //         const provider = new GithubAuthProvider();
+    //         await signInWithPopup(auth, provider);
+
+    //         console.log(auth.currentUser)
+    //         // if (auth.currentUser) {
+    //         //     const collectionRef = collection(db, auth.currentUser.email, {});
+    //         //     await addDoc(collectionRef);
+
+    //         // }
+    //         // create a new user document on firestore
+
+    //         // Fetch invoices for GitHub user
+    //         // Add your logic to fetch invoices from Firestore
+    //     } catch (error) {
+    //         console.error('Error signing in with GitHub:', error);
+    //     }
+    // };
     const handleSignInWithGitHub = async () => {
         try {
-            // Sign in with GitHub
             const provider = new GithubAuthProvider();
-            await signInWithPopup(auth, provider);
-            // Fetch invoices for GitHub user
-            // Add your logic to fetch invoices from Firestore
+            const result = await signInWithPopup(auth, provider);
+            setUser(result.user);
         } catch (error) {
             console.error('Error signing in with GitHub:', error);
         }
     };
+    useEffect(() => {
+        if (user) {
+            // If the user is signed in, fetch invoices from Firestore
+            const fetchInvoices = async () => {
+                try {
+                    const invoicesRef = collection(db, 'invoices');
+                    const q = query(invoicesRef, where('userId', '==', user.uid));
+
+                    const querySnapshot = await getDocs(q);
+                    const invoicesData = querySnapshot.docs.map((doc) => doc.data());
+                    console.log(invoicesData)
+                    setInvoices(invoicesData);
+                } catch (error) {
+                    console.error('Error fetching invoices:', error);
+                }
+            };
+            fetchInvoices();
+            //   console.log(Invoice)
+        }
+    }, [user]);
+
+
 
     return (
         <div>
--- src/store/AppContext.jsx
+++ src/store/AppContext.jsx
@@ -1,17 +1,17 @@
 import { createContext, useState } from "react";
-
+import { auth } from "../../public/firebase/FirebaseConfig";
 export const AppContext = createContext(null);
 
 
 export const AppContextProvider = ({ children }) => {
     const [invoices, setInvoices] = useState([])
-
+    console.log(auth.currentUser)
     const value = {
         invoices,
         setInvoices
     }
 
-
+    console.log(invoices)
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>;
