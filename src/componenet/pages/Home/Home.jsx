a9fb9817c8fc64bf8308dfe216a853f9e467b36e
badr ghanbi <bghanbi50@gmail.com>
2023-08-02 13:47:21 +0100

    create the new invoice yet componenet

--- src/componenet/pages/Home/Home.jsx
+++ src/componenet/pages/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import FilterInvoicesBar from "./FilterInvoicesBar/FilterInvoicesBar"
 import InvoicesList from './invoicesList/InvoicesList'
 import Container from '../../ui/Container'
-
+import NoInvoiceYet from '../../ui/NoInvoiceYet'
 const Home = () => {
     return (
         <Container>
             <FilterInvoicesBar />
             <InvoicesList />
+            <NoInvoiceYet />
         </Container>
     )
 }
---
+++ src/componenet/ui/NoInvoiceYet.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import illustrationEmpty from "../../../public/assets/illustration-empty.svg"
+
+const NoInvoiceYet = () => {
+    return (
+        <div className='mt-20 text-center'>
+            <img className='mx-auto' src={illustrationEmpty} alt="illustration-empty" />
+            <h2 className='my-3'>There is nothing here</h2>
+            <p className='text-[#888eb0]'>
+                Create an invoice by clicking the New Invoice button and get started
+            </p>
+        </div >
+    )
+}
+
+export default NoInvoiceYet
