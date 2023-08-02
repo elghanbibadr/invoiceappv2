f425cf94c9fa5259af16ffde2e8f46585c7c86fe
badr ghanbi <bghanbi50@gmail.com>
2023-08-02 13:51:07 +0100

    render the noinvoicetet componenet conditionally

--- src/componenet/pages/Home/Home.jsx
+++ src/componenet/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import FilterInvoicesBar from "./FilterInvoicesBar/FilterInvoicesBar"
 import InvoicesList from './invoicesList/InvoicesList'
 import Container from '../../ui/Container'
 import NoInvoiceYet from '../../ui/NoInvoiceYet'
+import { AppContext } from '../../../store/AppContext'
 const Home = () => {
+    const { Invoices } = useContext(AppContext)
+
     return (
         <Container>
             <FilterInvoicesBar />
-            <InvoicesList />
-            <NoInvoiceYet />
+            {Invoices?.length !== 0 && <InvoicesList />}
+            {Invoices?.length === 0 && <NoInvoiceYet />}
         </Container>
     )
 }
--- src/componenet/ui/NoInvoiceYet.jsx
+++ src/componenet/ui/NoInvoiceYet.jsx
@@ -6,9 +6,7 @@ const NoInvoiceYet = () => {
         <div className='mt-20 text-center'>
             <img className='mx-auto' src={illustrationEmpty} alt="illustration-empty" />
             <h2 className='my-3'>There is nothing here</h2>
-            <p className='text-[#888eb0]'>
-                Create an invoice by clicking the New Invoice button and get started
-            </p>
+            <p className='text-[#888eb0]'>Create an invoice by clicking the New Invoice button and get started </p>
         </div >
     )
 }
