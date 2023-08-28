27d3c8dba95ccc9fc80a4758b99a8c57b0a3fd76
badr ghanbi <bghanbi50@gmail.com>
2023-08-28 14:55:41 +0100

    install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+
+
   ],
   theme: {
     extend: {
@@ -18,6 +21,9 @@ export default {
       }
     },
   },
-  plugins: [],
+
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
 
