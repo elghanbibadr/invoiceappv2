484a1a7f2feac88a083c44ad51a147007ec9ea4e
badr ghanbi <bghanbi50@gmail.com>
2023-08-28 15:27:01 +0100

    create a none working datepicker custom componenet

--- src/componenet/addNewInvoice/NewInvoiceForm.jsx
+++ src/componenet/addNewInvoice/NewInvoiceForm.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IconLeftArrow from "../../../public/assets/icon-arrow-left.svg"
 import { Link } from 'react-router-dom'
 import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import Modal from '../ui/Modal'
 
@@ -11,91 +12,93 @@ const NewInvoiceForm = () => {
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
+        console.log(selectedDate)
     };
     return (
-        <Modal>
-            <div className='bg-secondaryColor overflow-y-scroll px-6 pt-20 h-full md:w-1/2 left-10'>
-                <Link to="/home" className='flex mb-10 cursor-pointer  md:hidden items-center'>
-                    <img src={IconLeftArrow} alt="left arrow icon" />
-                    <p className='font-bold mx-3'>Go Back</p>
-                </Link>
+        // <Modal>
+        <div className='bg-secondaryColor overflow-y-scroll px-6 pt-20 h-full md:w-1/2 left-10'>
+            <Link to="/home" className='flex mb-10 cursor-pointer  md:hidden items-center'>
+                <img src={IconLeftArrow} alt="left arrow icon" />
+                <p className='font-bold mx-3'>Go Back</p>
+            </Link>
 
-                {/*------------ FORM ------- */}
-                <h4 className='text-2xl md:text-3xl'>New Invoice</h4>
-                {/* ----bill form ----- */}
-                <form >
-                    <h6 className='text-accentColor my-6 font-bold'> Bill From</h6>
+            {/*------------ FORM ------- */}
+            <h4 className='text-2xl md:text-3xl'>New Invoice</h4>
+            {/* ----bill form ----- */}
+            <form >
+                <h6 className='text-accentColor my-6 font-bold'> Bill From</h6>
+                {/* input field */}
+                <div className="form-inputField">
+                    <label htmlFor="street">Street address</label>
+                    <input type="text" name="street" id="street" />
+                </div>
+                {/* gridx3 */}
+                <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
                     {/* input field */}
                     <div className="form-inputField">
-                        <label htmlFor="street">Street address</label>
-                        <input type="text" name="street" id="street" />
-                    </div>
-                    {/* gridx3 */}
-                    <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
-                        {/* input field */}
-                        <div className="form-inputField">
-                            <label htmlFor="country">Country</label>
-                            <input type="text" name="country" id="country" />
-                        </div>
-                        {/* input field */}
-                        <div className="form-inputField">
-                            <label htmlFor="city">City</label>
-                            <input type="text" name="city" id="city" />
-                        </div>
-                        {/* input field */}
-                        <div className="form-inputField col-span-2 md:col-span-1">
-                            <label htmlFor="postcode">Post Code</label>
-                            <input type="text" name="postcode" id="postcode" />
-                        </div>
-                    </div>
-                    <h6 className='text-accentColor my-6 font-bold'> Bill To</h6>
-                    {/* input field */}
-                    <div className="form-inputField ">
-                        <label htmlFor="ClientName">Client's Name</label>
-                        <input type="text" name="clientName" id="clientName" />
-                    </div>
-                    {/* input field */}
-                    <div className="form-inputField ">
-                        <label htmlFor="ClientEmail">Client's Email</label>
-                        <input type="text" name="clientEmail" id="clientEmail" />
-                    </div>
-                    {/* input field */}
-                    <div className="form-inputField ">
-                        <label htmlFor="ClientName">Client's Name</label>
-                        <input type="text" name="clientName" id="clientName" />
+                        <label htmlFor="country">Country</label>
+                        <input type="text" name="country" id="country" />
                     </div>
                     {/* input field */}
                     <div className="form-inputField">
-                        <label htmlFor="clientstreet">Street address</label>
-                        <input type="text" name="clientstreet" id="clientstreet" />
+                        <label htmlFor="city">City</label>
+                        <input type="text" name="city" id="city" />
                     </div>
-                    {/* gridx3 */}
-                    <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
-                        {/* input field */}
-                        <div className="form-inputField">
-                            <label htmlFor="clientcountry">Country</label>
-                            <input type="text" name="clientcountry" id="clientcountry" />
-                        </div>
-                        {/* input field */}
-                        <div className="form-inputField">
-                            <label htmlFor="clientcity">City</label>
-                            <input type="text" name="clientcity" id="clientcity" />
-                        </div>
-                        {/* input field */}
-                        <div className="form-inputField col-span-2 md:col-span-1">
-                            <label htmlFor="clientpostcode">Post Code</label>
-                            <input type="text" name="clientpostcode" id="clientpostcode" />
-                        </div>
+                    {/* input field */}
+                    <div className="form-inputField col-span-2 md:col-span-1">
+                        <label htmlFor="postcode">Post Code</label>
+                        <input type="text" name="postcode" id="postcode" />
                     </div>
-                    {/* date picker */}
-                    <DatePicker
-                        selected={selectedDate}
-                        onChange={handleDateChange}
-                        dateFormat="MM/dd/yyyy"
-                    />
-                </form>
-            </div>
-        </Modal>
+                </div>
+                <h6 className='text-accentColor my-6 font-bold'> Bill To</h6>
+                {/* input field */}
+                <div className="form-inputField ">
+                    <label htmlFor="ClientName">Client's Name</label>
+                    <input type="text" name="clientName" id="clientName" />
+                </div>
+                {/* input field */}
+                <div className="form-inputField ">
+                    <label htmlFor="ClientEmail">Client's Email</label>
+                    <input type="text" name="clientEmail" id="clientEmail" />
+                </div>
+                {/* input field */}
+                <div className="form-inputField ">
+                    <label htmlFor="ClientName">Client's Name</label>
+                    <input type="text" name="clientName" id="clientName" />
+                </div>
+                {/* input field */}
+                <div className="form-inputField">
+                    <label htmlFor="clientstreet">Street address</label>
+                    <input type="text" name="clientstreet" id="clientstreet" />
+                </div>
+                {/* gridx3 */}
+                <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+                    {/* input field */}
+                    <div className="form-inputField">
+                        <label htmlFor="clientcountry">Country</label>
+                        <input type="text" name="clientcountry" id="clientcountry" />
+                    </div>
+                    {/* input field */}
+                    <div className="form-inputField">
+                        <label htmlFor="clientcity">City</label>
+                        <input type="text" name="clientcity" id="clientcity" />
+                    </div>
+                    {/* input field */}
+                    <div className="form-inputField col-span-2 md:col-span-1">
+                        <label htmlFor="clientpostcode">Post Code</label>
+                        <input type="text" name="clientpostcode" id="clientpostcode" />
+                    </div>
+                </div>
+                {/* date picker */}
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={handleDateChange}
+                    className="custom-datepicker-container"
+                    dateFormat="MM/dd/yyyy"
+                />
+            </form>
+        </div>
+        // </Modal>
     )
 }
 
