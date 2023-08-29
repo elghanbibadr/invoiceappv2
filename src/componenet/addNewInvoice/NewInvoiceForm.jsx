5327f03bd9539f32507f1c971ae8f071756789a3
badr ghanbi <bghanbi50@gmail.com>
2023-08-29 15:10:07 +0100

    more customization on the calendar input

--- src/componenet/addNewInvoice/NewInvoiceForm.jsx
+++ src/componenet/addNewInvoice/NewInvoiceForm.jsx
@@ -104,13 +104,17 @@ const NewInvoiceForm = () => {
                             Invoice Date
 
                         </label>
-                        <input
-                            type="text"
-                            value={selectedDate.toDateString()}
-                            onClick={handleCalendarToggle}
-                            id='invoiceDate'
-                            readOnly
-                        />
+                        <div onClick={handleCalendarToggle}
+                            className='relative cursor-pointer'>
+                            <input
+                                type="text"
+                                value={selectedDate.toDateString()}
+                                id='invoiceDate'
+                                readOnly
+                            />
+                            <svg className='absolute right-4 top-[16px]' width="16" height="16" xmlns="http://www.w3.org/2000/svg" class="DatePicker_calendarIcon__3xhl5"><path fill="#7e88c3" d="M14 2h-.667V.667A.667.667 0 0012.667 0H12a.667.667 0 00-.667.667V2H4.667V.667A.667.667 0 004 0h-.667a.667.667 0 00-.666.667V2H2C.897 2 0 2.897 0 4v10c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm.667 12c0 .367-.3.667-.667.667H2A.668.668 0 011.333 14V6.693h13.334V14z" fill-rule="nonzero"></path></svg>
+
+                        </div>
                         {isCalendarOpen && (
                             <Calendar
                                 onChange={handleDateChange}
@@ -124,8 +128,8 @@ const NewInvoiceForm = () => {
                         )}
                     </div>
                 </form>
-            </div>
-        </Modal>
+            </div >
+        </Modal >
     )
 }
 
